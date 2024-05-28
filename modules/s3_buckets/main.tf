33a677971d90ba5f3c9290e2a27479868371260c
fatemeh.alizadeh <fatemeh.alizadeh@digikala.com>
2024-05-28 23:03:35 +0330

    remove configs that were not required

--- modules/s3_buckets/main.tf
+++ modules/s3_buckets/main.tf
@@ -2,3 +2,13 @@ resource "aws_s3_bucket" "test" {
   bucket  = "${var.bucket_name}"
 }
 
+resource "aws_s3_bucket" "terraform_state" {
+  bucket  = "${var.state_bucket_name}"
+}
+
+resource "aws_s3_bucket_versioning" "enabled" {
+  bucket = aws_s3_bucket.terraform_state.id
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
--- modules/s3_buckets/variables.tf
+++ modules/s3_buckets/variables.tf
@@ -1,3 +1,9 @@
 variable "bucket_name" {
   description = "The name of my buckets"
 }
+
+variable "versioning" {
+  description = "Enable versioning for the bucket"
+  type        = string
+  default     = "Disabled"
+}
