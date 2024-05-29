a8931b55a750b6aec2a55e9a8a80a5cafa873afd
fatemeh.alizadeh <fatemeh.alizadeh@digikala.com>
2024-05-29 05:35:30 +0330

    add two new variables for remote state bucket

--- variables.tf
+++ variables.tf
@@ -8,4 +8,10 @@ variable "first_bucket" {
   description = "here is your first bucket"
 }
 
+variable "remote_state_bucket" {
+  type = string
+  description = "It is the name of the bucket that your terraform state files are gonna store"
+}
+
 variable "force_destroy" {}
+variable "prevent_destroy" {}
