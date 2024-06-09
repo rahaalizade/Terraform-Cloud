351c23ca0906adeafbed65e390393a48ef781f0c
fatemeh.alizadeh <fatemeh.alizadeh@digikala.com>
2024-06-09 14:55:19 +0330

    add route table for public subnet

--- main.tf
+++ main.tf
@@ -48,3 +48,17 @@ resource "aws_internet_gateway" "gw" {
     Name = "InternetGW"
   }
 }
+
+resource "aws_route_table" "rt" {
+  vpc_id = aws_vpc.my_vpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.gw.id
+  }
+
+
+  tags = {
+    Name = "Internet-RT"
+  }
+}
