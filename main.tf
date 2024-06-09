c7b4171c62725b1f2bf24b78b26474a0247c18e5
fatemeh.alizadeh <fatemeh.alizadeh@digikala.com>
2024-06-09 14:40:54 +0330

    add vpc and subnet

--- main.tf
+++ main.tf
@@ -1,9 +1,42 @@
-provider "aws" {
-  region = var.region
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
 }
 
-module "s3_bucket" {
-  source              = "./modules/s3_buckets"
-  remote_state_bucket = var.remote_state_bucket
-  force_destroy       = var.force_destroy
+provider "aws" {
+  region = "us-east-1"
+}
+
+data "aws_availability_zones" "available" {
+  state = "available"
+}
+
+resource "aws_vpc" "my_vpc" {
+  cidr_block       = "10.0.0.0/16"
+
+  tags = {
+    Name = "Raha VPC"
+  }
+}
+
+resource "aws_subnet" "sb01" {
+  vpc_id     = aws_vpc.my_vpc.id
+  cidr_block = "10.0.1.0/24"
+
+  tags = {
+    Name = "Subnet01"
+  }
+}
+
+resource "aws_subnet" "sb02" {
+  vpc_id     = aws_vpc.my_vpc.id
+  cidr_block = "10.0.2.0/24"
+
+  tags = {
+    Name = "Subnet02"
+  }
 }
---
+++ output.tf
@@ -0,0 +1,3 @@
+output "list_of_AZs" {
+  value = data.aws_availability_zones.available
+}
